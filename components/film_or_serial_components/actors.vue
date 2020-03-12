1668b45d3df05b138bf522e5d0ac858c2d6cc97e
Bekert <pepega@gmail.com>
2020-03-12 22:09:28 +0200

    add components with info about actors

---
+++ components/film_or_serial_components/actors.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <actorsCard 
+            v-for="director in actorsAndGroups.filter(el => el.type === 'director')" 
+            :key="director.id"
+            :name="director.name"
+            :ENname="director.ENname"
+        />
+    </div>
+</template>
+
+<script>
+import actorsCard from '../actors-card'
+export default {
+    components: {
+        actorsCard
+    },
+    data: () => ({
+        actorsAndGroups: [
+            {
+                id: 1,
+                type: 'director',
+                name: 'Хоакин Феникс',
+                ENname: 'JoaquinPhoenix'
+            },
+            {
+                id: 2,
+                type: 'screenwriter',
+                name: 'Хоакин Феникс',
+                ENname: 'JoaquinPhoenix'
+            },
+            {
+                id: 3,
+                type: 'actor',
+                name: 'Хоакин Феникс',
+                ENname: 'JoaquinPhoenix'
+            },
+            {
+                id: 4,
+                type: 'actor',
+                name: 'Хоакин Феникс',
+                ENname: 'JoaquinPhoenix'
+            },
+            {
+                id: 5,
+                type: 'actor',
+                name: 'Хоакин Феникс',
+                ENname: 'JoaquinPhoenix'
+            }
+        ]
+    })
+
+}
+</script>
