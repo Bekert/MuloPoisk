55df7c30aa8c07f1fc020e480daa8a46c1569fd0
Bekert <pepega@gmail.com>
2020-03-12 22:07:53 +0200

    add page with info about film or serials

---
+++ pages/_id.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-row class="ma-0 pa-0">
+            <v-col cols="12" md="5">
+                <posterAndRate/>
+            </v-col>
+            <v-col cols="12" md="7">
+                <info/>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+import info from '../components/film_or_serial_components/info'
+import posterAndRate from '../components/film_or_serial_components/poster-and-rate'
+export default {
+    components: {
+        info, posterAndRate
+    }
+}
+</script>
