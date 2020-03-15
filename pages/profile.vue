e9d6a3b38dc4c466ffc66bdafd3423ac0f694432
Bekert <moroz.bogdan@protonmail.com>
2020-03-15 20:28:28 +0200

    add profile page

---
+++ pages/profile.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <v-col cols="12" sm="6" class="ma-auto">
+            <v-list>
+                <v-list-item>
+                    <v-list-item-title>Настройки:</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-text-field placeholder="Никнейм"></v-text-field>
+                </v-list-item>
+                <v-list-item>
+                    <v-text-field placeholder="Почта"></v-text-field>
+                </v-list-item>
+            </v-list>
+        </v-col>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+
+    })
+}
+</script>
