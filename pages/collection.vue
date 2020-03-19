9aaee81998eb4292c68692ad9023a46f4ad15955
Bekert <moroz.bogdan@protonmail.com>
2020-03-19 21:19:09 +0200

    add sort options and date elements

--- pages/collection.vue
+++ pages/collection.vue
@@ -8,11 +8,15 @@
             <v-tab-item>
                 <div class="d-flex">
                     <v-col cols="12" lg="9" class="pa-0">
-                        <FilmOrSerialCard2/>
-                        <FilmOrSerialCard2 class="mt-4"/>
+                        <SortList class="my-3" v-if="$vuetify.breakpoint.xl || $vuetify.breakpoint.lg"/>
+                        <SortListMob class="my-3" v-else-if="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm || $vuetify.breakpoint.md"/>
+                        <div v-for="n in 3" :key="n" class="mt-4">
+                            <div class="display-2 font-weight-bold" :class="{'title ml-2': $vuetify.breakpoint.xs || $vuetify.breakpoint.sm}">2{{n}} февраля</div>
+                            <FilmOrSerialCard2 class="mt-4" v-for="n in 2" :key="n"/>
+                        </div>
                     </v-col>
-                    <v-col cols="0" lg="3" class="pa-0" v-if="$vuetify.breakpoint.xl || $vuetify.breakpoint.lg">
-                        <div>pepega</div>
+                    <v-col cols="0" lg="3" class="pa-0 pl-2" v-if="$vuetify.breakpoint.xl || $vuetify.breakpoint.lg">
+                        <Filters/>
                     </v-col>
                 </div>
             </v-tab-item>
@@ -24,13 +28,16 @@
 </template>
 
 <script>
+import SortList from '../components/collection-components/SortList'
 import FilmOrSerialCard2 from '../components/FilmOrSerialCard2'
+import SortListMob from '../components/collection-components/SortListMob'
+import Filters from '../components/collection-components/Filters'
 export default {
     data: () => ({
         tab: null
     }),
     components: {
-        FilmOrSerialCard2
+        FilmOrSerialCard2, SortList, SortListMob, Filters
     }
 }
 </script>
